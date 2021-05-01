f700f823caa2ed7480abe3691733e77288bd29cc
EC2 Default User <ec2-user@ip-172-31-89-160.ec2.internal>
2021-05-01 15:44:19 +0000

    fix main.js

--- main.js
+++ main.js
@@ -21,25 +21,18 @@ $(function(){
     function updateTimetText(){
         
         var h = Math.floor(elapsedTime/3600000);
-        //m(分) = 135200 / 60000ミリ秒で割った数の商　-> 2分
+
         var m = Math.floor(elapsedTime / 60000);
 
-        //s(秒) = 135200 % 60000ミリ秒で / 1000 (ミリ秒なので1000で割ってやる) -> 15秒
         var s = Math.floor(elapsedTime % 60000 / 1000);
 
-        //ms(ミリ秒) = 135200ミリ秒を % 1000ミリ秒で割った数の余り
         var ms = elapsedTime % 1000;
 
-
-        //HTML 上で表示の際の桁数を固定する　例）3 => 03　、 12 -> 012
-        //javascriptでは文字列数列を連結すると文字列になる
-        //文字列の末尾2桁を表示したいのでsliceで負の値(-2)引数で渡してやる。
         h = ('0' + h).slice(-1);
         m = ('0' + m).slice(-2); 
         s = ('0' + s).slice(-2);
         ms = ('0' + ms).slice(-2);
 
-        //HTMLのid　timer部分に表示させる　
         timer.textContent = h + ':' + m + ':' + s + ':' + ms;
     }
 
